b00339c0e4b7cb29a64a3ad202d67328f4856e1e
Blackmokujingal <bea800.w98@gmail.com>
2023-03-05 20:42:35 +0400

    Started the backend with MongoDB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+
 dotenv.config();
 
 const app = express();
@@ -10,10 +12,17 @@ app.use(express.json({ limit: '50mb' }));
 
 app.get('/', async (req,res) => {
     res.send ('Hello from Dall-E ! ');
-})
+})  
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    try { 
+        connectDB(process.env.NONGODB_URL);
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    } catch (error) {
+        console.log(error);
+
+    }
+    
 }
 
 startServer();
