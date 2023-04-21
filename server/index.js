d632de762a2d4a93fa73790d05303d71129e2f41
Beatrice Washington <bea800.w98@gmail.com>
2023-04-21 22:10:11 +0400

    started the routing of the backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
@@ -10,16 +12,19 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('api/v1/post', postRoutes)
+app.use('api/v1/dalle', dalleRoutes)
+
 app.get('/', async (req,res) => {
     res.send ('Hello from Dall-E ! ');
 })  
 
 const startServer = async () => {
-    try { 
-        connectDB(process.env.NONGODB_URL);
-        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    try {
+      connectDB(process.env.MONGODB_URL);
+      app.listen(8000, () => console.log('Server started on port 8000'));
     } catch (error) {
-        console.log(error);
+      console.log(error);
 
     }
     
